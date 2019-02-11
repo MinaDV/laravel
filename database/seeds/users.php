40b0d3022e791b159badb4e3879ff1e29909eb8b
Leila Djelali <l.djelali@ats.local>
2019-02-12 00:30:54 +0100

    seed and migration

---
+++ database/seeds/users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+        $faker= Faker\Factory::create('ar_SA');
+        $data=[];
+        DB::table('users')->truncate();
+
+        for($i=1; $i <=100; $i++){
+            array_push($data,[
+                'name'=> $faker->name,
+                'email'=>$faker->email,
+                'password'=> bcrypt(123456),
+            ]);
+        }
+        DB::table('users')->insert($data);
+}
+}
